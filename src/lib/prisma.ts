bb66ab55f7d2d4a3caf00f78dbfa3163be0a96cc
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-07-08 10:28:20 -0300

    Move prisma client to node_modules

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma-client'
+import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma }
